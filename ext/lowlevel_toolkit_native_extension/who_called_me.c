65509116336ec90b02092b9319dc942e3df25ce7
Ivo Anjo <ivo.anjo@datadoghq.com>
2025-04-08 13:01:04 +0100

    Minor simplification in who_called_me

--- ext/lowlevel_toolkit_native_extension/who_called_me.c
+++ ext/lowlevel_toolkit_native_extension/who_called_me.c
@@ -1,11 +1,11 @@
 #include <ruby/ruby.h>
 #include <ruby/debug.h>
 
-static VALUE callback(const rb_debug_inspector_t *dbg_context, void *data) {
-  bool return_self = (data != NULL);
-  return return_self ?
+static VALUE callback(const rb_debug_inspector_t *dbg_context, void *return_self) {
+  return (return_self != NULL) ?
     // Depth: 0 -> us; 1 -> caller; 2 -> caller of caller
-    rb_debug_inspector_frame_self_get(dbg_context, 2) : rb_debug_inspector_frame_binding_get(dbg_context, 2);
+    rb_debug_inspector_frame_self_get(dbg_context, 2) :
+    rb_debug_inspector_frame_binding_get(dbg_context, 2);
 }
 
 static VALUE who_called_me(RB_UNUSED_VAR(VALUE _)) { return rb_debug_inspector_open(callback, (void *) !NULL); }
